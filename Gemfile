d8fb97c2dc3b32825fb028efb332217b73b41138
Nickolay Kondratenko <devmarkup@gmail.com>
2014-10-02 19:28:03 +0300

    add rspec simplecov and factory_girl

--- Gemfile
+++ Gemfile
@@ -38,3 +38,11 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :test  do
+  gem 'simplecov', require: false
+end
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module KontNoor
+module WebLog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -26,5 +26,8 @@ module KontNoor
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.factory_girl dir: 'spec/factories'
+    end
   end
 end
