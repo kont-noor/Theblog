3722518ee553f62dfc4e28c41ca8ff8047bada14
Nickolay Kondratenko <devmarkup@gmail.com>
2015-03-11 08:07:43 +0200

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Yablog::Engine.routes.draw do
   devise_for :users, class_name: "Yablog::User", module: :devise
-  resources :pages
-  resources :posts, only: :show
+
   get '/:slug' => 'content_nodes#show', :as => :root_content_node
   get '/:category/:slug' => 'content_nodes#show', :as => :content_node
   root 'home#index'
